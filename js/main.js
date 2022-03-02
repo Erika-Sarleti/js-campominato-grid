8c797a99244253104bf5ec6b3c90d14deb8adeb7
ErikaSarleti <erikasarleti00@gmail.com>
2022-03-02 16:57:53 +0100

    javascript (non completo)

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 */
+
+const selDifficulty = document.getElementById('select-opt');
+const btnPlay =  document.getElementById('playbtn');
+let containerBox = document.querySelector('.container');
+
+btnPlay.addEventListener('click',function(){
+    function createGrid(max){
+        // let square;
+        let level = selDifficulty.value;
+        // containerBox.innerHTML += square;
+        for (i = 1; i <= max; i++){
+            containerBox.innerHTML += `<div class="square">${i}</div>`; 
+        }    
+        if (level == easy){
+            createGrid(49);
+        } else if (level == medium){
+            createGrid(81);
+        } else{
+            createGrid(100);
+        }
+    }
+})
